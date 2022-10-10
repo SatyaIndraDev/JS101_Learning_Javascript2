1658db42e79877829e89f3680a51f21fafad3f9a
SatyaIndraDev <satyaindrad@gmail.com>
2022-10-10 16:10:02 +0530

    variable

---
+++ Introduction-to-JS-Variables-and-Data-Types/p2.js
@@ -0,0 +1,13 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Satya indra dev"
+console.log(name)
+ name="P r Arya"
+console.log(name)
+ name="Pushpa Arya"
+console.log(name)
---
+++ Introduction-to-JS-Variables-and-Data-Types/p4.js
@@ -0,0 +1,16 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let name="Satya indra dev"
+let school=(`
+█▀▄▀█ ▄▀█ █▀ ▄▀█ █   █▀ █▀▀ █░█ █▀█ █▀█ █░░
+█░▀░█ █▀█ ▄█ █▀█ █   ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄`);
+  
+  let section="A"
+    let roll_no=1
+      let phymarks=70
+let chemmarks=80
+let mathsmarks=90
+console.log("Name"+"-"+name+"\n"+"School"+"-"+school+"\n"+"section"+"-"+section+"\n"+"roll no"+"-"+roll_no+"\n"+"phymarks"+"-"+phymarks+"\n"+"chemmarks"+"-"+chemmarks+"\n"+"mathsmarks"+"-"+mathsmarks)
