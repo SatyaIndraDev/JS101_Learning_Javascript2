4903e3225907a14802046a49abfe075db2d83f70
SatyaIndraDev <satyaindrad@gmail.com>
2022-10-10 16:17:24 +0530

    Day2

---
+++ Javascript-IV-Conditional-Statements-IWn/p3.js
@@ -0,0 +1,13 @@
+// Problem 3: Given 2 numbers a and b print which is greater or "both equal".
+
+let a=10
+let b=20
+if(a>b){
+  console.log("A")
+}
+else if(b>a){
+  console.log("B")
+}
+else {
+  console.log("both equal")
+}
---
+++ Javascript-IV-Conditional-Statements-IWn/p4.js
@@ -0,0 +1,13 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let username="satyaindradev"
+let password="qwerty"
+let user="satyaindradev"
+let pass="qwerty"
+if(username==user && password==pass){
+  console.log("login successfully")
+}
+
+else {
+  console.log("Print valid id and pass")
+}
